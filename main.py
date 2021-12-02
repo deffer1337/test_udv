a24b19dc7171387a44dc6c6be85452bb7b500328
igor <ighor.naumov.2002mail@mail.ru>
2021-12-02 16:26:47 +0500

    delete contextvar. add app['redis']

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import os
-from contextvars import ContextVar
 
 from aiohttp import web
 from aioredis import create_redis_pool
 
 from converter_currency.routes import create_routes
 
-redis_connection = ContextVar('redis_connection')
-
 
 async def on_cleanup(app):
-    redis_conn = redis_connection.get()
+    redis_conn = app['redis']
     redis_conn.close()
     await redis_conn.wait_closed()
 
 
 async def init():
     redis_conn = await create_redis_pool(os.getenv('ADDRESS'))
-    redis_connection.set(redis_conn)
-    routes = create_routes(redis_connection.get())
+    routes = create_routes(redis_conn)
     app = web.Application()
+    app['redis'] = redis_conn
     app.add_routes(routes)
     app.on_cleanup.append(on_cleanup)
     return app
