f7f50b5abd605c5aa41d5ffe6c405ab8712ea6ff
igor <ighor.naumov.2002mail@mail.ru>
2021-12-02 16:34:26 +0500

    update tests_currency_storage.py

--- tests/tests_currency_storage.py
+++ tests/tests_currency_storage.py
@@ -9,9 +9,11 @@ class TestCurrencyStorage:
     def setup_class(self):
         self.currency_storage = CurrencyStorage(StorageMock())
 
+    @pytest.mark.asyncio
     async def test_get_when_currency_RUR_than_return_one(self):
         assert await self.currency_storage.get('RUR') == 1
 
+    @pytest.mark.asyncio
     async def test_get_when_currency_not_exists_than_raise_value_error(self):
         with pytest.raises(ValueError) as exception:
             currency = 'BOB'
@@ -24,16 +26,19 @@ class TestCurrencyStorage:
         await self.currency_storage.update({'USD': 60.0}, True)
         assert await self.currency_storage.get('USD') == 60.0
 
+    @pytest.mark.asyncio
     async def test_update_when_merge_false_then_old_data_should_be_removed(self):
         await self.currency_storage.update({'USD': 60.0, 'EUR': 20}, True)
         await self.currency_storage.update({'RUR': 120.0}, False)
         assert list(self.currency_storage._redis.db.keys()) == ['RUR']
 
+    @pytest.mark.asyncio
     async def test_update_when_merge_true_then_old_data_should_be_save(self):
         await self.currency_storage.update({'USD': 60.0, 'EUR': 20}, True)
         await self.currency_storage.update({'RUR': 120.0}, True)
         assert set(self.currency_storage._redis.db.keys()) == {'USD', 'EUR', 'RUR'}
 
+    @pytest.mark.asyncio
     async def test_update_when_merge_true_and_currency_exists_in_db_then_old_currency_should_be_change(self):
         await self.currency_storage.update({'USD': 60.0}, True)
         await self.currency_storage.update({'USD': 120.0}, True)
